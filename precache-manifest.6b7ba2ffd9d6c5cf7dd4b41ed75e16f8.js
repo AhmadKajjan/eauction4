ef93329ce81901763838087e37a12e1fc011721a
Ahmad-Kajjan <blaketear@hotmail.com>
2020-06-17 04:36:32 -0700

    Updates

---
+++ precache-manifest.6b7ba2ffd9d6c5cf7dd4b41ed75e16f8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ce803433421f557134369091a3eef3",
+    "url": "/eauction4/index.html"
+  },
+  {
+    "revision": "4011ae5896c0df815e07",
+    "url": "/eauction4/static/css/main.1c207834.chunk.css"
+  },
+  {
+    "revision": "b5521b9ba154ca78697b",
+    "url": "/eauction4/static/js/2.8114bd75.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/eauction4/static/js/2.8114bd75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4011ae5896c0df815e07",
+    "url": "/eauction4/static/js/main.e0dbcc17.chunk.js"
+  },
+  {
+    "revision": "01721cd245af3f3b4b29",
+    "url": "/eauction4/static/js/runtime-main.0060259c.js"
+  },
+  {
+    "revision": "0a089d7739adfd33d2c452596739ae75",
+    "url": "/eauction4/static/media/item1.0a089d77.jpg"
+  },
+  {
+    "revision": "c68c6122e5a1e29b2234c4f8d3e6c1b8",
+    "url": "/eauction4/static/media/item2.c68c6122.jpg"
+  },
+  {
+    "revision": "86fef79ab4192076182c5e8238676820",
+    "url": "/eauction4/static/media/item3.86fef79a.jpg"
+  }
+]);
